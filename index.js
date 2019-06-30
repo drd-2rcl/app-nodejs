76092bcb4f2eaefaf46180f84e07e64c7ee8553e
drd-2rcl <drd.rclrd@gmail.com>
2019-06-30 13:20:55 -0300

    was created route for create new users

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const server = express();
 
-const users = ['Diogo', 'Carol', 'Leandro'];
+server.use(express.json());
+
+const users = ['Diogo', 'Carol', 'Carlos'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
-})
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
 
 server.listen(3000);
