0d6820e39e4a67cd45871ae898d1596137cbea9b
drd-2rcl <drd.rclrd@gmail.com>
2019-06-30 13:01:12 -0300

    was create route for exemple in users

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'First route' });
+const users = ['Diogo', 'Carol', 'Leandro'];
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
 })
 
 server.listen(3000);
